434f183364b0cf436dafefd517ac417d0b5798a6
Sion Ramos de los Santos <sionandrieux@hotmail.com>
2024-12-19 10:55:00 -0300

    temp

--- app/(client)/cart/actions.ts
+++ app/(client)/cart/actions.ts
@@ -3,10 +3,6 @@
 import 'server-only';
 import { orderCtrl } from '@/app/lib/compose/inversify';
 
-export async function addProductAction(data: IAddProductAction) {
-  return await orderCtrl.addItem(data);
-}
-
 export async function deleteProductAction(data: IDeleteProductAction) {
   return await orderCtrl.removeItem(data);
 }
@@ -15,9 +11,4 @@ interface ICartAtion {
   userId: number;
   productId: number;
 }
-
 interface IDeleteProductAction extends ICartAtion {}
-
-interface IAddProductAction extends ICartAtion {
-  quantity: number;
-}
