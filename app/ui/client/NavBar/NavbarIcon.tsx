01b7c1c3155c8da0e9af6f951c904fa7268c2c79
Sion Ramos de los Santos <sionandrieux@hotmail.com>
2025-04-12 06:35:46 -0300

    fix some styles

--- app/ui/client/NavBar/NavbarIcon.tsx
+++ app/ui/client/NavBar/NavbarIcon.tsx
@@ -4,9 +4,15 @@ import { INFO_STATIC } from '@/app/lib/static';
 
 export function NavBarIcon() {
   const [tipo, name] = INFO_STATIC.name.split(' ');
+
+  const mainStyle = 'sm:text-sm md:text-lg m-auto mx-2 font-semibold';
+  const spanStyle = 'hidden sm:inline';
+  const span2Style = 'inline sm:hidden';
+
   return (
-    <Link className="text-lg m-auto mx-2 font-semibold" href={CLIENT_PATH.HOME}>
-      <span className="hidden sm:inline">{tipo}</span> {name}
+    <Link className={mainStyle} href={CLIENT_PATH.HOME}>
+      <span className={spanStyle}>{tipo}</span>
+      <span className={span2Style}>{tipo[0]}. </span> {name}
     </Link>
   );
 }
--- app/ui/client/NavBar/index.tsx
+++ app/ui/client/NavBar/index.tsx
@@ -3,12 +3,20 @@ import { NavBarIcon } from './NavbarIcon';
 import { NavBarHandler } from './NavBarHandler';
 import { NavbarProvider } from './navbarContext';
 import { getSession } from '@/app/lib/session';
+import clsx from 'clsx';
 
 export async function Navbar() {
   const session = await getSession();
 
+  const className = clsx(
+    'flex',
+    'h-[2.5rem] w-screen px-[1rem]',
+    'items-center justify-between',
+    'bg-brand-light',
+  );
+
   return (
-    <nav className="flex h-[2.5rem] w-screen px-[1rem] bg-brand-light items-center justify-between">
+    <nav className={className}>
       <NavBarIcon />
       <NavbarSearch />
       <div className="flex">
